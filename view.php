be2578bf45107565499255991287b25039b9e053
krish-drc <m.krishnanath0716@gmail.com>
2025-09-15 01:09:57 +0530

    Day 3 - Display entries with PHP + search + table

--- view.php
+++ view.php
@@ -25,7 +25,7 @@ $result = $conn->query($sql);
 <head>
  <meta charset="UTF-8">
  <meta name="viewport" content="width=device-width, initial-scale=1">
- <title>View Entries - Your Project</title>
+ <title>View Entries - Library Management Sysytem</title>
  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
